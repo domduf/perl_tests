78fa1ae2e8e5615420f57e04ec97fa354926916f
domduf <minique.duf@gmail.com>
2017-09-23 11:59:38 +0200

    amelioration et modes diferents d entree

--- regex.pl
+++ regex.pl
@@ -4,42 +4,74 @@ use strict;
 
 use warnings;
 
-print "entrez une chaine\n";
-my $chaine = <>;
-chomp $chaine;
 
-#my $chaine= "salut ça va 234 ?";
+
+# declaration des variables
+my $chaine;
 my $reg;
 my $resultat;
 my $boucle = "";
+my $bascule = -1;
+
+
+
 
 while ( $boucle eq "" ){
-	print "entrez la regex : \n";
-	$reg = <>;
-	chomp $reg;
 
-	
+
+
+	if ($bascule < 0 or $bascule == 1) {
+
+		#------CHAINE à TESTER -----
+		(print "-MODE REGEX (on change la chaine)\n"."/".$reg."/\n")if ($bascule ==1);
+		print "entrez une chaine\n";
+		$chaine = <>;
+		chomp $chaine;
+	}
+
+	if ($bascule < 0 or $bascule == 0) {
+		#---------- REGEX ----------
+		(print "-MODE CHAINE (on change la regex)\n".$chaine."\n" )if ($bascule == 0);		
+		print "entrez la regex : \n";
+		$reg = <>;
+		chomp $reg;
+	}
+
+
+
+	#--- CALCUL DU RESULTAT ----
 	$resultat = ( $chaine =~  m/$reg/ ); #m/$reg/
 
+
+	#---- AFFICHAGE ------------
 	print "chaine: ".$chaine."\n".
-	"regex: \"".$reg."\"\n";
+	"regex: /".$reg."/\n";
 	
 	# affichage du resultat
 	(print "resultat: ".$resultat."\n") if ( $resultat ne "" );
 
+	$bascule= abs($bascule);
 
-	print "\nAppuyez sur une touche differente de ENTREE pour sortir,\net sur 'a' pour changer la chaine à tester\n";
+
+	#----- MENU ----------------
+
+	print "------------------------------------------\n";
+	print "-MODE CHAINE " if ($bascule == 0);
+	print "-MODE REGEX " if ($bascule ==1);
+	print "\n- a pour changer le mode de saisie.\n";
+	print "- autre touche pour sortir.\n";
+	print "Appuyez sur ENTREE\n";
+
+	print "------------------------------------------\n";
+
 	$boucle = <>;
 	chomp $boucle;
 
 	if ($boucle eq "a") {
 		$boucle = "";
-		print "Entrez la nouvelle chaine à tester : \n";
-		$chaine = <>;
-		chomp $chaine;
+		$bascule = abs($bascule-1);
 	}
-
-
+	
 	 
 }
 
